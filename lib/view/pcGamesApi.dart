a0fe9bf943dce5ada797e9584a7b23b17916155b
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-27 18:15:22 +0530

    Added Game Layout

--- lib/view/pcGamesApi.dart
+++ lib/view/pcGamesApi.dart
@@ -18,12 +18,61 @@ class _MyPcGamesApiState extends State<MyPcGamesApi> {
       ),
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color(0xFF3D3F54)),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF3D3F54)),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(
+                      "https://www.freetogame.com/g/516/thumbnail.jpg",
+                      width: 120,
+                      height: 120,
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5),
+                        Text(
+                          "PUBG: BATTLEGROUNDS",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Get into the action in one of the longest running battle royale games PUBG Battlegrounds.",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "PC (Windows)",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Developer : KRAFTON, Inc.",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Releae Date : 2022-01-12",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
